25f3b7bbf15e9af85c92f8b28762100e0cd207de
YUN HU <asdfg20232jp@gmail.com>
2020-01-24 11:39:21 +0800

    read file

--- products.py
+++ products.py
@@ -1,4 +1,13 @@
 products = []
+#讀取檔案
+with open('products.csv', 'r', encoding='big5') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(products)
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱: ')
     if name == 'q':
@@ -6,6 +15,11 @@ while True:
     price = input('請輸入商品價格: ')
     products.append([name, price])
 print(products)
-
+#列印出清單明細
 for p in products:
     print(p[0], '的價格是', p[1], '元')
+#寫入檔案
+with open('products.csv', 'w', encoding= 'big5') as f:
+	f.write('商品,價格\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
